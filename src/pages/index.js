327b26d2e014fc9dffc311bfed759b231629a4d5
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-16 00:42:02 -0400

    some other minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,39 +7,60 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <div style={{
-      display: "flex",
-    }}>
-      <div style={{
-        width: "48%",
-        paddingRight: "2%",
-      }}>
-    <div style={{
-      width: "100%",
-      fontSize: "60px",
-      paddingTop: "12px",
-      textAlign: "right",
-      color: "black",
-      // color: "rgb(216, 100, 52)",
-      // textShadow: "2px 2px black",
-    }}>Brian Fragnoli</div>
     <div
-    style={{
-      fontSize: "45px",
-      textAlign: "right",
-      paddingTop: "25px",
-      // textShadow: "2px 2px rgb(216, 100, 52)",
-    }}>Freelance Photographer</div>
-    <div style={{
-      textAlign: "right",
-      fontSize: "24px",
-    }}>I am a freelance photographer currently working on my certification using a `equipment`.  I specialize in ...</div>
-    </div>
-    <img src="https://i.imgur.com/w0PoPDx.png" style={{
-      width: "40%",
-      borderRadius: "45px",
-      border: "4px solid black"
-    }} />
+      style={{
+        display: "flex",
+      }}
+    >
+      <div
+        style={{
+          width: "48%",
+          paddingRight: "2%",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            fontSize: "60px",
+            paddingTop: "12px",
+            textAlign: "right",
+            color: "black",
+            // color: "rgb(216, 100, 52)",
+            // textShadow: "2px 2px black",
+          }}
+        >
+          Brian Fragnoli
+        </div>
+        <div
+          style={{
+            fontSize: "45px",
+            textAlign: "right",
+            paddingTop: "25px",
+            // textShadow: "2px 2px rgb(216, 100, 52)",
+          }}
+        >
+          Freelance Photographer
+        </div>
+        <div
+          style={{
+            textAlign: "right",
+            fontSize: "24px",
+          }}
+        >
+          I am a freelance photographer currently working on my certification
+          using a `equipment`. I specialize in still life and sports photography
+          and would like to kickstart a career in sideline sports images. Don't
+          worry I summitted that climb.
+        </div>
+      </div>
+      <img
+        src="https://i.imgur.com/w0PoPDx.png"
+        style={{
+          width: "40%",
+          borderRadius: "45px",
+          border: "4px solid black",
+        }}
+      />
     </div>
   </Layout>
 )
