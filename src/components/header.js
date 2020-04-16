c1a285871e08ce894e4e0f92253fb2da8f738d0e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-15 20:39:13 -0400

    getting home icon in the nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,26 @@
 // import PropTypes from "prop-types"
 import React from "react"
 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+library.add(fab, fas, far)
+
+const Nav = styled.div`
+  background: rgb(113, 84, 58);
+  /* border-top: 24px solid black;
+  border-bottom: 24px solid black; */
+  margin: 0;
+`
+
 const Header = ({ siteTitle }) => (
-  <div style={{
-    backgroundColor: "rgb(113, 84, 58)",
-    color: "rgb(216, 100, 52)",
-    height: "100px",
-  }}>
-efhasdif
-  </div>
+  <Nav>
+    <FontAwesomeIcon icon={["fas", "camera-retro"]} size="3x" />
+    <div>efhasdif</div>
+  </Nav>
 )
 
 export default Header
