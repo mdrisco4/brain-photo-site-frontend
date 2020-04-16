5182425e2bfb0507b228033026d2af04ae6b29bd
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-16 00:47:21 -0400

    edits on email link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,45 @@
 import React from "react"
+import styled from "styled-components"
+
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
+const EmailLink = styled.a`
+  color: black;
+  text-decoration: none;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  font-size: 45px;
+  margin-top: 24px;
+  font-weight: bold;
+  text-shadow: 2px 2px rgb(216, 100, 52);
+  &:hover {
+    color: rgb(216, 100, 52);
+    text-shadow: 2px 2px black;
+  }
+`
+
 const About = () => (
   <Layout>
-    <div style={{
+    <div
+      style={{
         textAlign: "center",
         width: "80%",
         fontSize: "56px",
         marginLeft: "auto",
         marginRight: "auto",
-    }}>
+      }}
+    >
       If you have any questions anout any of my work or would like to work with
       me please contact me at:
     </div>
-    <a href="mailto:beefrags1982@gmail.com" target="_blank"
-      style={{
-          color: "black",
-          textDecoration: "none",
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-        textAlign: "center",
-        fontSize: "45px",
-        marginTop: "24px",
-        fontWeight: "bold",
-        textShadow: "1px 1px rgb(216, 100, 52)",
-      }}
-    >
+    <EmailLink href="mailto:beefrags1982@gmail.com" target="_blank">
       beefrags1982@gmail.com
-    </a>
+    </EmailLink>
     <div
       style={{
         //   display: "inline-block",
