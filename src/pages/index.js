c549c8eb7fbf691ab99463d6d9086d300ef5d187
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-16 00:39:26 -0400

    some other minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,16 @@ const IndexPage = () => (
       textAlign: "right",
       paddingTop: "25px",
       // textShadow: "2px 2px rgb(216, 100, 52)",
-    }}>Freelance Photography</div>
+    }}>Freelance Photographer</div>
+    <div style={{
+      textAlign: "right",
+      fontSize: "24px",
+    }}>I am a freelance photographer currently working on my certification using a `equipment`.  I specialize in ...</div>
     </div>
     <img src="https://i.imgur.com/w0PoPDx.png" style={{
       width: "40%",
+      borderRadius: "45px",
+      border: "4px solid black"
     }} />
     </div>
   </Layout>
