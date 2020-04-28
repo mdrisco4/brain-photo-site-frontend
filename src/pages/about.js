8a551f2f4c2d1fe5e8d586fe39e3bdf5d4673f5e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-28 15:30:31 -0400

    did about page in styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,31 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
+const Section = styled.div`
+  text-align: center;
+  font-size: 52px;
+  margin-top: 20px;
+`
+
+const SectionInfo = styled.div`
+  text-align: center;
+  font-size: 36px;
+  margin-top: 20px;
+`
+
 const About = () => (
   <Layout>
-    <div style={{
-      textAlign: "center",
-      fontSize: "52px",
-      marginTop: "20px",
-    }}>About Me</div>
-    <div style={{
-      textAlign: "center",
-      fontSize: "36px",
-      marginTop: "20px",
-    }}>A whole buncha stuff and some back story</div>
-    <div style={{
-      textAlign: "center",
-      fontSize: "52px",
-      marginTop: "20px",
-    }}>Hobbies and Interests</div>
-     <div style={{
-      textAlign: "center",
-      fontSize: "36px",
-      marginTop: "20px",
-    }}>Climbing mountains, camping, etc.</div>
+    <Section>About Me</Section>
+    <SectionInfo>A whole buncha stuff and some back story</SectionInfo>
+    <Section>Hobbies and Interests</Section>
+    <SectionInfo>Climbing mountains, camping, etc.</SectionInfo>
   </Layout>
 )
 
