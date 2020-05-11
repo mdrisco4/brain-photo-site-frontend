35f490de39d9570ff879158243c3312bc04389b5
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-05-11 13:43:34 -0400

    getting back to work on connecting the backend

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,8 +5,54 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
-const url =
-  "https://cdn.contentful.com/spaces/hkey7bq8d8xc/entries?access_token=y0u9tyuxRS9TnyYKQ_yhIyIIaWkOj0mb1TS6yKl7VUk"
+var contentful = require('contentful')
+var client = contentful.createClient({
+    space: 'hkey7bq8d8xc',
+    accessToken: 'bRB059jwbB4MphUkzedacei1ZQze8uxraRhh-pDSVIQ'
+  })
+
+// const url =
+//   "https://cdn.contentful.com/spaces/hkey7bq8d8xc/entries?access_token=y0u9tyuxRS9TnyYKQ_yhIyIIaWkOj0mb1TS6yKl7VUk"
+
+
+//   ??
+//   client.getAssets()
+//   .then(function (assets) {
+//     assets.items.map(function(asset){
+//       var imageURL = url;
+//     });
+//   })
+//   .catch(function (e) {
+//     console.log(e);
+//   });
+
+
+
+
+//SINGLE ENTRY 
+
+client.getEntry('20Bpyxs7Mswdx3cMBijZ8f')
+.then(function (entry) {
+  // logs the entry metadata
+  console.log(entry)
+
+  // logs the field with ID title
+  console.log(entry.fields.productName)
+})
+
+
+
+//ALL ENTRIES IN SPACE
+
+// client.getEntries()
+// .then(function (entries) {
+//   // log the title for all the entries that have it
+//   entries.items.forEach(function (entry) {
+//     if(entry.fields.productName) {
+//       console.log(entry.fields.productName)
+//     }
+//   })
+// })
 
 const PhotoGrid = styled.div`
   display: grid;
