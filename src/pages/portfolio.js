c4c27626ecac68f6b7da5f2cdd3e5abe6088576c
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-05-12 15:37:39 -0400

    setting up layout on portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,27 +1,34 @@
 import React from "react"
 import styled from "styled-components"
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
 export const ImageQuery = graphql`
-    query MyQuery {
-        allContentfulAsset {
-        nodes {
-            title
-            fluid {
-            src
-            }
+  query MyQuery {
+    allContentfulAsset {
+      nodes {
+        title
+        description
+        fluid {
+          src
         }
-        }
+      }
     }
-  `
+  }
+`
 
 const PhotoGrid = styled.div`
   display: grid;
   grid-template-rows: 1fr 1fr;
   grid-template-columns: 1fr 1fr 1fr;
+`
+
+const ItemContainer = styled.div``
+
+const Title = styled.div`
+  text-align: center;
 `
 
 const Photo = styled.img`
@@ -31,28 +38,45 @@ const Photo = styled.img`
   margin-right: 20%;
 `
 
+const Description = styled.div`
+  text-align: center;
+`
+
 const Portfolio = props => {
-    const image = props.data.allContentfulAsset.nodes[0].title
-    const photo = props.data.allContentfulAsset.nodes[0].fluid.src
-    // const photo = props.data.allContentfulAsset.nodes[0].fluid.src
-    // const photo = props.data.allContentfulAsset.nodes[0].fluid.src
-    // const photo = props.data.allContentfulAsset.nodes[0].fluid.src
-    // const photo = props.data.allContentfulAsset.nodes[0].fluid.src
+  const title = props.data.allContentfulAsset.nodes[0].title
+  const photo = props.data.allContentfulAsset.nodes[0].fluid.src
+  const description = props.data.allContentfulAsset.nodes[0].description
 
-
-return (
-  <Layout>
-    <p>{image}</p>
-    <PhotoGrid>
-    <Photo src={photo} />
-      <Photo src="https://i.imgur.com/CosJK9o.jpg" />
-      <Photo src="https://i.imgur.com/tu0FRyn.jpg" />
-      <Photo src="https://i.imgur.com/wSqiQim.jpg" />
-      <Photo src="https://i.imgur.com/n8JRfj5.jpg" />
-      <Photo src="https://i.imgur.com/nMt8jsA.jpg" />
-    </PhotoGrid>
-  </Layout>
-)
+  return (
+    <Layout>
+      <PhotoGrid>
+        <ItemContainer>
+          <Title>{title}</Title>
+          <Photo src={photo} />
+          <Description>{description}</Description>
+        </ItemContainer>
+        <ItemContainer>
+          <Photo src="https://i.imgur.com/CosJK9o.jpg" />
+        </ItemContainer>
+        <ItemContainer>
+          <Title></Title>
+          <Photo src="https://i.imgur.com/tu0FRyn.jpg" />
+        </ItemContainer>
+        <ItemContainer>
+          <Title></Title>
+          <Photo src="https://i.imgur.com/wSqiQim.jpg" />
+        </ItemContainer>
+        <ItemContainer>
+          <Title></Title>
+          <Photo src="https://i.imgur.com/n8JRfj5.jpg" />
+        </ItemContainer>
+        <ItemContainer>
+          <Title></Title>
+          <Photo src="https://i.imgur.com/nMt8jsA.jpg" />
+        </ItemContainer>
+      </PhotoGrid>
+    </Layout>
+  )
 }
 
 export default Portfolio
