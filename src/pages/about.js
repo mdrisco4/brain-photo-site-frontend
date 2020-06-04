5af19eb5a5c8fdc8d0087fa154c8e5ab531bec18
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-06-04 19:24:27 -0400

    finishing up the about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,10 @@ import Layout from "../components/layout"
 const Section = styled.div`
   text-align: center;
   font-size: 52px;
-  margin-top: 20px;
+  padding-top: 10%;
   font-family: cursive;
+  font-weight: bold;
+  text-shadow: 1px 1px white;
 `
 
 const SectionInfo = styled.div`
@@ -16,14 +18,26 @@ const SectionInfo = styled.div`
   font-size: 36px;
   margin-top: 20px;
   font-family: cursive;
+  font-weight: bold;
+  width: 70%;
+  margin: 0 15%;
+  text-shadow: 1px 1px white;
+`
+
+const AboutPagePic = styled.div`
+  background-image: url('http://images.ctfassets.net/hkey7bq8d8xc/j9OtMb9mMxOAv3izjnGTL/1ce54aa61bdd0abc81129f9c6ce4beb1/DSCN1173.jpg');
+  background-size: cover;
+  padding-bottom: 900px;
 `
 
 const About = () => (
   <Layout>
+    <AboutPagePic>
     <Section>About Me</Section>
-    <SectionInfo>A whole buncha stuff and some back story</SectionInfo>
+    <SectionInfo>Born in Texas, I have lived in the greater Rochester area for the past 25 years.  I try to reamin active in the community and currently reside in Fairport.</SectionInfo>
     <Section>Hobbies and Interests</Section>
     <SectionInfo>Climbing mountains, camping, etc.</SectionInfo>
+    </AboutPagePic>
   </Layout>
 )
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,9 +8,23 @@ const ContactPrompt = styled.div`
 font-family: 'Krona One', sans-serif;
   text-align: center;
   width: 80%;
-  font-size: 56px;
+  font-size: 24px;
   margin-left: auto;
   margin-right: auto;
+  padding-top: 5%;
+  max-width: 1200px;
+  @media (min-width: 575px) {
+    font-size: 32px;
+  }
+  @media (min-width: 768px) {
+    font-size: 39px;
+  }
+  @media (min-width: 950px) {
+    font-size: 45px;
+  }
+  @media (min-width: 1200px) {
+    font-size: 52px;
+  }
 `
 
 const EmailLink = styled.a`
@@ -21,7 +35,7 @@ font-family: 'Krona One', sans-serif;
   margin-left: auto;
   margin-right: auto;
   text-align: center;
-  font-size: 45px;
+  font-size: 20px;
   margin-top: 24px;
   font-weight: bold;
   text-shadow: 2px 2px white;
@@ -29,17 +43,44 @@ font-family: 'Krona One', sans-serif;
     color: white;
     text-shadow: 2px 2px black;
   }
+  @media (min-width: 540px) {
+    font-size: 27px;
+  }
+  @media (min-width: 585px) {
+    font-size: 32px;
+  }
+  @media (min-width: 768px) {
+    font-size: 39px;
+  }
+  @media (min-width: 950px) {
+    font-size: 45px;
+  }
+  @media (min-width: 1200px) {
+    font-size: 52px;
+  }
 `
 
 const Info = styled.div`
 font-family: 'Krona One', sans-serif;
   text-align: center;
-  font-size: 45px;
+  font-size: 24px;
   margin-top: 24px;
   font-weight: bold;
+  @media (min-width: 575px) {
+    font-size: 32px;
+  }
+  @media (min-width: 768px) {
+    font-size: 39px;
+  }
+  @media (min-width: 950px) {
+    font-size: 45px;
+  }
+  @media (min-width: 1200px) {
+    font-size: 52px;
+  }
 `
 
-const FrontPagePic = styled.div`
+const ContactPagePic = styled.div`
   background-image: url('http://images.ctfassets.net/hkey7bq8d8xc/2d2siJGRM2EXx3MMe8N7bn/00f27a790b6ea402db28d6a05942bd27/DSCN0415.JPG');
   background-size: cover;
   padding-bottom: 400px;
@@ -47,7 +88,7 @@ const FrontPagePic = styled.div`
 
 const About = () => (
   <Layout>
-    <FrontPagePic>
+    <ContactPagePic>
     <ContactPrompt>
       If you have any questions about any of my work and want to know more
       please contact me at:
@@ -57,7 +98,7 @@ const About = () => (
     </EmailLink>
     <Info>(585) 267-9609</Info>
     <Info>Perinton, NY 14450</Info>
-    </FrontPagePic>
+    </ContactPagePic>
   </Layout>
 )
 
